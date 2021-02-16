b61bb1a6064a26ac269c8f053dcb985ca2ae9f71
Psylo2 <psylo20@hotmail.com>
2021-02-16 12:09:28 +0200

    taking care of bytes in postgresSQL

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
 from security import authenticate, identity
-from resources.user import UserRegister, UserList,
+from resources.user import UserRegister, UserList, User
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
