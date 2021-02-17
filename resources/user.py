95e0828be9c1bf31e60031d04dfc71a852719518
Psylo2 <psylo20@hotmail.com>
2021-02-17 12:50:43 +0200

    Fixed JWT_Extended

--- resources/user.py
+++ resources/user.py
@@ -1,7 +1,7 @@
 from flask_jwt_extended import create_access_token, create_refresh_token, jwt_required
 from flask_restful import Resource, reqparse
 from models.user import UserModel
-from db.db import insert_timestamp,decrypt
+from db.db import insert_timestamp, decrypt
 
 class UserRegister(Resource):
     parser = reqparse.RequestParser()
