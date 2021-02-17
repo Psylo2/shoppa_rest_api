857e74789d4ec04fe34664b63b7a94aea24d096e
Psylo2 <psylo20@hotmail.com>
2021-02-17 17:06:10 +0200

    Fixed JWT

--- security.py
+++ security.py
@@ -3,9 +3,8 @@ import bcrypt
 
 def authenticate(username, password):
     user = UserModel.find_by_username(username)
-    if user:
-        if decrypt(username, user.hash_username) and decrypt(password, user.password):
-            return user
+    if user and decrypt(password, user.password):
+        return user
 
 def identity(payload):
     user_id = payload['identity']
