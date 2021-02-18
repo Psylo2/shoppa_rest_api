9d7ec9ae005e3fd60c2edb20d4b3908eb173d834
Psylo2 <psylo20@hotmail.com>
2021-02-18 16:49:10 +0200

    Improve DB, resources, models, uwsgi

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///db/data.db')
 app.config['PROPAGATE_EXCEPTIONS'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.secret_key = 'pablo'
+app.secret_key = 'your_secret_password'
 api = Api(app)
 
 jwt = JWT(app, authenticate, identity)
