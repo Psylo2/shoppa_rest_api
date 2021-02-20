9eeb2d2275484b611cb79401cdcf2b407080315c
Psylo2 <psylo20@hotmail.com>
2021-02-20 23:34:56 +0200

    Added Block List, User Cart & Security

--- security.py
+++ security.py
@@ -1,26 +1,23 @@
 from models.user import UserModel
-import bcrypt
-from db.db import encrypt, insert_timestamp
+from models.blocklist import BlockListModel
+from db.db import encrypt, insert_timestamp, decrypt
 
 def authenticate(content, password):
     user = UserModel.find_by_username(content)
     if user:
         if decrypt(content, encrypt(user.username)) and decrypt(password, user.password):
-            user.last_login_timestamp = insert_timestamp()
-            user.save_to_db()
-            return user
+            if (BlockListModel.find_by_user_id(user.id)) is None:
+                user.last_login_timestamp = insert_timestamp()
+                user.save_to_db()
+                return user
     user = UserModel.find_by_email(content)
     if user:
         if decrypt(content, encrypt(user.email)) and decrypt(password, user.password):
-            user.last_login_timestamp = insert_timestamp()
-            user.save_to_db()
-            return user
+            if (BlockListModel.find_by_user_id(user.id)) is None:
+                user.last_login_timestamp = insert_timestamp()
+                user.save_to_db()
+                return user
 
 def identity(payload):
     user_id = payload['identity']
     return UserModel.find_by_id(user_id)
-
-
-def decrypt(x, y):
-    return bcrypt.checkpw(x.encode("UTF-8"), y)
-
