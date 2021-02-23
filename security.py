1989db766a6c863bad6998472168e46ae7665a1f
Psylo2 <psylo20@hotmail.com>
2021-02-23 07:53:00 +0200

    Added requirements.txt and security.py

--- security.py
+++ security.py
@@ -5,7 +5,7 @@ from db.db import encrypt, insert_timestamp, decrypt
 def auth_by_username(content: str, password: str):
     user = UserModel.find_by_username(content)
     if user:
-        if decrypt(password, user.password):
+        if decrypt(content, encrypt(user.username)) and decrypt(password, user.password):
             if (BlockListModel.find_by_user_id(user.id)) is None:
                 user.last_login_timestamp = insert_timestamp()
                 user.save_to_db()
@@ -15,7 +15,7 @@ def auth_by_username(content: str, password: str):
 def auth_by_email(content: str, password: str):
     user = UserModel.find_by_email(content)
     if user:
-        if decrypt(password, user.password):
+        if decrypt(content, encrypt(user.email)) and decrypt(password, user.password):
             if (BlockListModel.find_by_user_id(user.id)) is None:
                 user.last_login_timestamp = insert_timestamp()
                 user.save_to_db()
